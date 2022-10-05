5af90f43a0f1b394e8341a0546fd9ccfa231b5c3
Dennis <dennisgachomo@gmail.com>
2022-10-05 18:10:11 +0300

    4th commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,13 +10,15 @@ const Content = () => {
   return (
     <div className ='content-container'>
         <div className='tabs'>
-            <div className='tab-leagues'>
+            <div className='tab-leagues' onClick={()=>serActive(true)}>
                 <h2>Leagues</h2>
 
             </div>
-            <div className='tab-standings'>
+            <div className='tab-standings' onClick={()=>serActive(false)}>
                 <h2>Standings</h2>
             </div>
+
+            {active ? <Leagues/> : <Standings/>}
         </div>
       
     </div>
