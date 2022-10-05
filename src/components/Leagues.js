a5be0627458675b756d3bcb06b560bc7aa30557a
Dennis <dennisgachomo@gmail.com>
2022-10-05 21:11:08 +0300

    8th commit

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+import axios from 'axios';
 
 function Leagues() {
-    return (
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        axios('https://api-football-standings.azharimm.site/leagues').then(res=>{
+            console.log(res.data.data);
+            setData(res.data.data);
+        }
+        );
+
+    },[]);
+
+
+    return 
         <div className='leagues-container'>
-            leagues
+            
 
-        </div>
-    );
-}
+        </div>;
+    
+};
 
-export default Leagues
+export default Leagues;
