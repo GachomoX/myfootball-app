b029296634db0c8021dbd7c36c582221c0f2893b
Dennis <dennisgachomo@gmail.com>
2022-10-05 17:37:36 +0300

    2nd commit

---
+++ src/components/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css'
+const Content = () => {
+  return (
+    <div className ='content-container'>
+        <div className='tabs'>
+            <div className='tab-leagues'>
+                <h2>Leagues</h2>
+
+            </div>
+            <div classname='tab-standings'>
+                <h2>Standings</h2>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <h1>Football App ⚽</h1>
+      
+    </div>
+  )
+}
+
+export default NavBar
