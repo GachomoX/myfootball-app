8800a442f256a7da5876f485a49082d8015bf31e
Dennis <dennisgachomo@gmail.com>
2022-10-07 01:38:35 +0300

    12th commit

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,6 +1,6 @@
 import React, {useState,useEffect} from "react";
 import '../App.css';
-import axios from "axios";
+//import axios from "axios";
 
 
 
@@ -10,23 +10,36 @@ const Leagues = () => {
     const [data,setData]=useState([]);
 
     useEffect(()=>{
-        axios('https://api-football-standings.azharimm.site/leagues').then(res=>{
-            console.log(res.data);
-            setData(res.data.data);
-        }
         
-    );
+            
+        
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '82f711ab14msh5fc9f050e3530c3p1944f6jsnc5c1a506b958',
+                'X-RapidAPI-Host': 'football-prediction-api.p.rapidapi.com'
+            }
+        };
+        
+        fetch('https://football-prediction-api.p.rapidapi.com/api/v2/get-list-of-fixture-ids', options)
+            .then(response => response.json())
+            .then(response => setData(response))
+            .catch(err => console.error(err));
 
     },[])
+    console.log(data)
+
+    
 
 
 
 
   return (
     <div className='leagues-container'>
-        {data.map((data)=>{
-            <div></div>
-        })}
+        {data.map(()=>(
+            <div>{data}</div>
+        ))}
+        
       
     </div>
   )
