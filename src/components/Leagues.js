cd3580a625cdb1a6b51c0a2c357cdef334228ffe
Dennis <dennisgachomo@gmail.com>
2022-10-06 21:17:21 +0300

    10th commit

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -4,24 +4,27 @@ import axios from "axios";
 
 
 
-const Leagues = ()=>{
+const Leagues = () => {
+    
 
-    const [data,setData] = useState([]);
+    const [data,setData]=useState([]);
 
     useEffect(()=>{
         axios('https://api-football-v1.p.rapidapi.com/v3/teams/seasons').then(res=>{
             console.log(res.data);
-        });
+        })
 
-    },[]);
+    },[])
 
 
-    return
-        <div className="leagues-container">
-            leagues
 
-        </div>;
-    
+
+  return (
+    <div className='leagues-container'>
+        leagues
+      
+    </div>
+  )
 }
 
 export default Leagues
