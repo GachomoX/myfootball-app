fe2cd04956c7fb1974db46f177964b5026082a7a
Dennis <dennisgachomo@gmail.com>
2022-10-06 20:33:27 +0300

    8th commit

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,25 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState,useEffect} from "react";
 import '../App.css';
-import axios from 'axios';
+import axios from "axios";
 
-function Leagues() {
+
+
+const Leagues = ()=>{
+
     const [data,setData] = useState([]);
+
     useEffect(()=>{
-        axios('https://api-football-standings.azharimm.site/leagues').then(res=>{
-            console.log(res.data.data);
-            setData(res.data.data);
-        }
-        );
+        axios('https://api-football-v1.p.rapidapi.com/v3/teams/seasons').then(res=>{
+            console.log(res.data);
+        });
 
     },[]);
 
 
-    return 
-        <div className='leagues-container'>
-            
+    return
+        <div className="leagues-container">
+            leagues
 
         </div>;
     
-};
+}
 
-export default Leagues;
+export default Leagues
